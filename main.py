5187bbd23046f59fe03450d5843dc2c20e618f7e
pygamegrupo-eng <pygamegrupo@gmail.com>
2025-11-14 12:30:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,25 @@ Alto=600
 Fps=60
 
 pygame.init()
-pygame.display.set_caption("FALPY BIRD ")
+pygame.display.set_caption("Gemoetri dash ")
 screen =pygame.display.set_mode((Ancho,Alto))
 clock=pygame.time.Clock()
+
+def mostrar_menu():
+  print("n\=====Menu====")
+  print("1.Jugar")
+  print("2.opciones")
+  print("3.Salir")
+def main ():
+  while True:
+    mostrar_menu()
+    opcion=input("seleciona una opcion")
+
+    if opcion=="1":
+      print("Iniciando el juegon\n")
+      #logica del juego poner aqui
+    else opcion
+      
 
 ejecutando=True
 while ejecutando:
@@ -21,3 +37,11 @@ while ejecutando:
   clock.tick(Fps)
 
 pygame.quit()
+
+
+
+
+
+
+
+
